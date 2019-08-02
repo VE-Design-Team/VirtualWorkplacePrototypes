7d3f9bef640f67d63492d2254b57b32b0b415f6d
nchand <nikhil.chand@rmit.edu.au>
2019-08-02 16:16:34 +1000

    MessageBoard update

--- docs/CDE-ACF/final/js/script.js
+++ docs/CDE-ACF/final/js/script.js
@@ -39,47 +39,104 @@
 const login = () => {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
- 
-    if(username === '' || password==='') 
-    alert("Login credentials cannot be Empty!")
-    else if(username === 'PCAstudent' && password==='C4DE_learn')
-    {
-       window.location.href = 'messageboard.html';
+
+    if (username === '' || password === '')
+        alert("Login credentials cannot be Empty!")
+    else if (username === 'PCAstudent' && password === 'C4DE_learn') {
+        window.location.href = 'messageboard.html';
     }
     else {
-       alert("Incorrect username or password! ")
+        alert("Incorrect username or password! ")
     }
 }
+
+//Function to display LoginInfo
 const loginInfo = () => {
-    alert("Your Username is: 'PCAstudent' \nPassword is:'C4DE_learn' ")
+    alert("Your Username is: 'PCAstudent' \nPassword is:'C4DE_learn'")
+}
+
+//Print current date on messageBoard
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth() + 1; //January is 0!
+let yyyy = today.getFullYear();
+
+if (dd < 10) {
+    dd = '0' + dd
+}
+
+if (mm < 10) {
+    mm = '0' + mm
+}
+
+// today = yyyy + '/' + mm + '/' + dd;
+today = yyyy + '/' + mm + '/' + dd;
+todayDate = dd + '-' + mm + '-' + yyyy
+
+// console.log(today);
+document.getElementById('inlineFormInput').value = today;
+document.getElementById('inlineFormInputGroup').value = today;
+document.getElementById('inbox1').innerHTML = todayDate;
+document.getElementById('inbox2').innerHTML = todayDate;
+
+//Prints the current local time in messageBoard
+(() => {
+    const checkTime = (i) => {
+        return (i < 10) ? "0" + i : i;
+    }
+
+    const startTime = () => {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds());
+        document.getElementById('time1').innerHTML = h + ":" + m
+        document.getElementById('time2').innerHTML = h + ":" + m
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+})();
+
+messageOne = () => document.getElementById("customCheck1").checked = true;
+
+messageTwo = () => document.getElementById("customCheck2").checked = true;
+
+//function to navigate to record page if both messages are read
+route = () => {
+    if (document.getElementById("customCheck1").checked && document.getElementById("customCheck2").checked)
+        window.location.href = 'foodrecord.html';
+    else
+        alert("Read all message to proceed")
 }
 //function to add rows to table in foodrecordPage
-let count=1
+let count = 1
 const addRow = () => {
     count++
-    if(count<=10){
-    let row = document.getElementById("rowToClone") // find row to replicate
-    let table = document.getElementById("tableToModify") // find table to append to
-    let clone = row.cloneNode(true); // copy children 
-    // clone.id = "newID"; // change id or other attributes/contents
-    // table.appendChild(clone); // add new row to end of tabl
-    let InputType = clone.getElementsByClassName("foodrecord")
+    if (count <= 10) {
+        let row = document.getElementById("rowToClone") // find row to replicate
+        let table = document.getElementById("tableToModify") // find table to append to
+        let clone = row.cloneNode(true); // copy children 
+        // clone.id = "newID"; // change id or other attributes/contents
+        // table.appendChild(clone); // add new row to end of tabl
+        let InputType = clone.getElementsByClassName("foodrecord")
 
-    for (let i=0; i<InputType.length; i++){
-     if( InputType[i].name=='role'){
-        // InputType[i].type=='checkbox'
-        //  InputType[i].checked = false; 
-         //&&InputType[i].value!=' ' 
-    }else{
-       InputType[i].value=''           
-       }
+        for (let i = 0; i < InputType.length; i++) {
+            if (InputType[i].name == 'role') {
+                // InputType[i].type=='checkbox'
+                //  InputType[i].checked = false; 
+                //&&InputType[i].value!=' ' 
+            } else {
+                InputType[i].value = ''
+            }
+        }
+        table.appendChild(clone)
     }
-    table.appendChild(clone)
-}
-else {
-    alert("Row limit exceeded")
-}
+    else {
+        alert("Row limit exceeded")
+    }
 
-  }
+}
 
 

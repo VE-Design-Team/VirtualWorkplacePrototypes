f931b69d037c48433177d92ed8497b589821449f
Lawrence Makoona <lawrence.makoona@rmit.edu.au>
2019-08-06 09:46:48 +1000

    Merge remote-tracking branch 'origin/master'

--- docs/CDE-ACF/final/js/save_localstorage.js
+++ docs/CDE-ACF/final/js/save_localstorage.js
@@ -1,88 +1,119 @@
 tableData = []
 newArray = []
-arr=[]
+arr = []
 //newTable=[]
 const pdf = () => {
-tableToJson()
-var doc = new jsPDF();
-let returnedTarget= Object.assign({}, tableData);
-  console.log("inside the new fun  ")
-   console.log(returnedTarget)
-  console.log( returnedTarget[Object.keys(returnedTarget)[0]] )
-      var col = [["Date","Time","Food Item","Amount Taken","Time Taken","Role"]];
-      //var col1 = ["Details", "Values"];
-      var rows = [];
-      var rows1 = [];
+    tableToJson()
+    var doc = new jsPDF();
+    let returnedTarget = Object.assign({}, tableData);
+    console.log("inside the new fun  ")
+    console.log(returnedTarget)
+    console.log(returnedTarget[Object.keys(returnedTarget)[0]])
+    var col = [["Date", "Time", "Food Item", "Amount Taken", "Time Taken", "Role"]];
+    //var col1 = ["Details", "Values"];
+    var rows = [];
+    var rows1 = [];
 
- /* The following array of object as response from the API req  */
+    /* The following array of object as response from the API req  */
 
 
-// returnedTarget.forEach(element => {      
-//      returnedTarget[Object.keys(returnedTarget)[0]]
-//         var temp = [element.date,element.time];
-//        // var temp1 = [element.id,element.name];
-     
-//         // rows.push(temp);
-//        // rows1.push(temp1);
-       
-//     });        
-       
-       for(let i=0;i<tableData.length;i++){
-       arr.push(returnedTarget[Object.keys(returnedTarget)[i]] )
-       rows.push(arr[i]);
-       }
-       console.log("Printing ARR")
-       console.log(arr)
-   
-       console.log("Printing rows")
-       console.log(rows)
-       // newRow=[]
-       // for(let j=0;j<rows.length;j++){
-       //     newRow.push(rows[j])
-       // }
-       let temparray
-       let chunk = 6; let len=rows.length;
-       for (let i=0; i<len; i+=chunk) {
-       temparray = rows.slice(i,i+chunk);
-       
-       newArray.push(temparray)
-       
-   // do whatever
+    // returnedTarget.forEach(element => {      
+    //      returnedTarget[Object.keys(returnedTarget)[0]]
+    //         var temp = [element.date,element.time];
+    //        // var temp1 = [element.id,element.name];
+
+    //         // rows.push(temp);
+    //        // rows1.push(temp1);
+
+    //     });        
+
+    for (let i = 0; i < tableData.length; i++) {
+        arr.push(returnedTarget[Object.keys(returnedTarget)[i]])
+        rows.push(arr[i]);
+    }
+    console.log("Printing ARR")
+    console.log(arr)
+
+    console.log("Printing rows")
+    console.log(rows)
+    // newRow=[]
+    // for(let j=0;j<rows.length;j++){
+    //     newRow.push(rows[j])
+    // }
+    let temparray
+    let chunk = 6; let len = rows.length;
+    for (let i = 0; i < len; i += chunk) {
+        temparray = rows.slice(i, i + chunk);
+
+        newArray.push(temparray)
+
+        // do whatever
+    }
+
+    doc.autoTable({ head: col, body: newArray });
+    //[['21-10-2001','00:30','Apple','None','00:30','PCA']]
+    //doc.autoTable(col1, rows1, { startY: 60 });
+    doc.save('Report.pdf');
+
 }
-      
-      doc.autoTable({head:col,body:newArray});
-       //[['21-10-2001','00:30','Apple','None','00:30','PCA']]
-       //doc.autoTable(col1, rows1, { startY: 60 });
-       doc.save('Report.pdf');
 
-}
-   
 
 
 function tableToJson() {
-   const myTab = document.getElementsByClassName( 'foodrecord' ),
-       //tableData = []
-       _length = myTab.length;
+    const myTab = document.getElementsByClassName('foodrecord'),
+        //tableData = []
+        _length = myTab.length;
 
-   for ( let i = 0; i < _length; i++ ) {
-       const _value = myTab[ i ].value,
-           _innerText = myTab[ i ].innerText;
-       if ( _value ) {
-           //console.log( _value );
-           tableData.push( _value )
-           //newTable.push(tableData)
-       } else if ( _innerText ) {
-           //console.log( _innerText );
-          tableData.push( _innerText )
-    
-           //newTable.push(tableData)
-       }
-      
-   }
-   
-   console.log( tableData );
-   // tableData = []
-  
+    for (let i = 0; i < _length; i++) {
+        const _value = myTab[i].value,
+            _innerText = myTab[i].innerText;
+        if (_value) {
+            console.log(_value);
+            tableData.push(_value)
+            //newTable.push(tableData)
+        } else if (_innerText) {
+            //console.log( _innerText );
+            tableData.push(_innerText)
+
+            //newTable.push(tableData)
+        }
+
+    }
+
+    //console.log( tableData );
+    // tableData = []
+
 }
 
+// tableDat = []
+// function cont() {
+//     const myTable = document.getElementsByClassName('contrecord'),
+//         //tableData = []
+//         _length_ = myTable.length;
 
+//     for (let i = 0; i < _length_; i++) {
+//         const _value_ = myTable[i].value,
+//             _innerText_ = myTable[i].innerText;
+//         if (_value_) {
+//             console.log(_value_);
+//             tableDat.push(_value_)
+//             //newTable.push(tableData)
+//         } else if (_innerText_) {
+//             //console.log( _innerText );
+//             tableDat.push(_innerText_)
+
+//             //newTable.push(tableData)
+//         }
+//         //console.log(tableDat)
+//     }
+//     //console.log(tableDat)
+//     slicearr()
+// }
+// temparray = []
+// slicearr = () => {
+// var i,chunk = 4;
+// for (i=0; i<4; i+=chunk) {
+//     temparray = tableDat.slice(i,i+chunk);
+// }
+// console.log("temprray" + " "+temparray)
+// }
--- docs/CDE-ACF/final/js/script.js
+++ docs/CDE-ACF/final/js/script.js
@@ -1,6 +1,6 @@
 // let timeTaken = document.getElementById("timeTaken")
 // let amountTaken = document.getElementById("amountTaken")
-
+//$('select').selectpicker();
 // //Create array of options to be added
 // const time = ['00:00','00:15','00:30',
 //               '00:45','01:00','01:15',
@@ -34,91 +34,117 @@
 //     option.text = amount[i]
 //     newSelectList.appendChild(option)
 // }
-//function to add rows to table in foodrecordPage
-let count=1
-const addRow = () => {
-    count++
-    if(count<=10){
-    let row = document.getElementById("rowToClone") // find row to replicate
-    let table = document.getElementById("tableToModify") // find table to append to
-    let clone = row.cloneNode(true); // copy children 
-    // clone.id = "newID"; // change id or other attributes/contents
-    // table.appendChild(clone); // add new row to end of tabl
-    let InputType = clone.getElementsByClassName("foodrecord")
 
-    for (let i=0; i<InputType.length; i++){
-     if( InputType[i].name=='role'||InputType[i].type=='checkbox'){
-        InputType[i].checked = false; 
-         //&&InputType[i].value!=' '
-       // InputType[i].checked = false;  
-    }else{
-       InputType[i].value=''           
-       }
+window.onload = function() {
+    if (window.location.href.indexOf('messageboard.html') > -1) {
+      currentDateAndTime();
     }
-    table.appendChild(clone)
-}
-else {
-    alert("Row limit exceeded")
-}
   }
 
-// const contTime = [  '00:00','01:00','02:00','03:00','04:00',
-//                     '05:00','06:00','07:00','08:00','09:00',
-//                     '10:00','11:00','12:00','13:00','14:00',
-//                     '15:00','16:00','17:00','18:00','19:00',
-//                     '20:00','21:00','22:00','23:00' ]
+//login form validation
+const login = () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-// for (let i = 0; i < contTime.length; i++) {
-//  var timeVal = document.getElementById("continTime")
-//  //var playerscore = document.getElementById('player' + (i + 1) + "score")
+    if (username === '' || password === '')
+        alert("Login credentials cannot be Empty!")
+    else if (username === 'PCAstudent' && password === 'C4DE_learn') {
+        window.location.href = 'messageboard.html';
+    }
+    else {
+        alert("Incorrect username or password! ")
+    }
+}
 
-//  timeVal.innerText = contTime[i]
+//Function to display LoginInfo
+const loginInfo = () => {
+    alert("Your Username is: 'PCAstudent' \nPassword is:'C4DE_learn'")
+}
 
-// }
+//Print current date on messageBoard
+currentDateAndTime = () => {
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth() + 1; //January is 0!
+let yyyy = today.getFullYear();
 
-// const highlightOption = ['1','2','3','4','4b','c']
-// let high = document.getElementsByClassName("highlight")
-// timeTaken.appendChild(selectList) //
-// let selectHighlight = document.createElement("select")
-// for (let i = 0; i < highlightOption.length; i++) {
-//     let option = document.createElement("option")
-//     option.setAttribute("value", highlightOption[i])
-//     option.text = highlightOption[i]
-//     selectHighlight.appendChild(option)
-// }
+if (dd < 10) {
+    dd = '0' + dd
+}
 
-// var expanded = false;
+if (mm < 10) {
+    mm = '0' + mm
+}
 
-// function showCheckboxes() {
-//   var checkboxes = document.getElementById("checkboxes");
-//   if (!expanded) {
-//     checkboxes.style.display = "block";
-//     expanded = true;
-//   } else {
-//     checkboxes.style.display = "none";
-//     expanded = false;
-//   }
-// }
+// today = yyyy + '/' + mm + '/' + dd;
+today = yyyy + '/' + mm + '/' + dd;
+todayDate = dd + '-' + mm + '-' + yyyy
 
-var options = [];
+// console.log(today);
+document.getElementById('inlineFormInput').value = today;
+document.getElementById('inlineFormInputGroup').value = today;
+document.getElementById('inbox1').innerHTML = todayDate;
+document.getElementById('inbox2').innerHTML = todayDate;
 
-$( '.dropdown-menu a' ).on( 'click', function( event ) {
+//Prints the current local time in messageBoard
+(() => {
+    const checkTime = (i) => {
+        return (i < 10) ? "0" + i : i;
+    }
 
-   var $target = $( event.currentTarget ),
-       val = $target.attr( 'data-value' ),
-       $inp = $target.find( 'input' ),
-       idx;
+    const startTime = () => {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds());
+        document.getElementById('time1').innerHTML = h + ":" + m
+        document.getElementById('time2').innerHTML = h + ":" + m
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+})();
+}
 
-   if ( ( idx = options.indexOf( val ) ) > -1 ) {
-      options.splice( idx, 1 );
-      setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
-   } else {
-      options.push( val );
-      setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
-   }
+messageOne = () => document.getElementById("customCheck1").checked = true;
 
-   $( event.target ).blur();
-      
-   console.log( options );
-   return false;
-});
+messageTwo = () => document.getElementById("customCheck2").checked = true;
+
+//function to navigate to record page if both messages are read
+route = () => {
+    if (document.getElementById("customCheck1").checked && document.getElementById("customCheck2").checked)
+        window.location.href = 'patientrecord.html';
+    else
+        alert("Read all message to proceed")
+}
+//function to add rows to table in foodrecordPage
+let count = 1
+const addRow = () => {
+    count++
+    if (count <= 10) {
+        let row = document.getElementById("rowToClone") // find row to replicate
+        let table = document.getElementById("tableToModify") // find table to append to
+        let clone = row.cloneNode(true); // copy children 
+        // clone.id = "newID"; // change id or other attributes/contents
+        // table.appendChild(clone); // add new row to end of tabl
+        let InputType = clone.getElementsByClassName("foodrecord")
+
+        for (let i = 0; i < InputType.length; i++) {
+            if (InputType[i].name == 'role') {
+                // InputType[i].type=='checkbox'
+                //  InputType[i].checked = false; 
+                //&&InputType[i].value!=' ' 
+            } else {
+                InputType[i].value = ''
+            }
+        }
+        table.appendChild(clone)
+    }
+    else {
+        alert("Row limit exceeded")
+    }
+
+}
+
+

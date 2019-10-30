d970194f2dcc9889594391cb79f50fd14d9538a8
Lawrence <lawrence.makoona@rmit.edu.au>
2019-10-30 13:33:31 +1100

    WHV VW V6

---
+++ docs/WHV/prototype/virtual_workplace/virtual_workplace_v06/wp-login.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>wordpress481 &lsaquo; Log In</title>
+	<style type="text/css">
+    #login h1 a,
+    .login h1 a {
+        background-image: url(https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v05/wp-content/themes/page-builder/img/rmit-login-logo.png);
+        height: 65px;
+        width: 320px;
+        background-size: 320px 65px;
+        background-repeat: no-repeat;
+        padding-bottom: 30px;
+    }
+</style>
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v05/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.11' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v05" title="An emedia resource" tabindex="-1">wordpress481</a></h1>
+	
+<form name="loginform" id="loginform" action="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v05/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v05/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v05/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+/**
+ * Filters whether to print the call to `wp_attempt_focus()` on the login screen.
+ *
+ * @since 4.8.0
+ *
+ * @param bool $print Whether to print the function call. Default true.
+ */
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v05/">&larr; Back to wordpress481</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	

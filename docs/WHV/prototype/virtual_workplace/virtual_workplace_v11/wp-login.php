302b86f6ca92f28bf851714c6011069cdf90306c
Lawrence <lawrence.makoona@rmit.edu.au>
2019-11-29 11:36:05 +1100

    VW - V11

--- docs/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-login.php
+++ docs/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-login.php
@@ -11,7 +11,7 @@
 	<style type="text/css">
     #login h1 a,
     .login h1 a {
-        background-image: url(https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-content/themes/page-builder/img/rmit-login-logo.png);
+        background-image: url(https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-content/themes/page-builder/img/rmit-login-logo.png);
         height: 65px;
         width: 320px;
         background-size: 320px 65px;
@@ -20,11 +20,11 @@
     }
 </style>
 <link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-includes/css/dashicons.min.css?ver=5.2.4' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-includes/css/buttons.min.css?ver=5.2.4' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-admin/css/forms.min.css?ver=5.2.4' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-admin/css/l10n.min.css?ver=5.2.4' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-admin/css/login.min.css?ver=5.2.4' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-includes/css/dashicons.min.css?ver=5.2.4' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-includes/css/buttons.min.css?ver=5.2.4' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-admin/css/forms.min.css?ver=5.2.4' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-admin/css/l10n.min.css?ver=5.2.4' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-admin/css/login.min.css?ver=5.2.4' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -33,9 +33,9 @@
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
-		<h1><a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10">An emedia resource</a></h1>
+		<h1><a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11">An emedia resource</a></h1>
 	
-	<form name="loginform" id="loginform" action="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -47,13 +47,13 @@
 			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-				<input type="hidden" name="redirect_to" value="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-admin/" />
+				<input type="hidden" name="redirect_to" value="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-admin/" />
 					<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 			<p id="nav">
-					<a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/wp-login.php?action=lostpassword">Lost your password?</a>
+					<a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/wp-login.php?action=lostpassword">Lost your password?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -70,7 +70,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v10/">
+			<p id="backtoblog"><a href="https://ve-design-team.github.io/VirtualWorkplace/WHV/prototype/virtual_workplace/virtual_workplace_v11/">
 		&larr; Back to wordpress481	</a></p>
 			
 	</div>
